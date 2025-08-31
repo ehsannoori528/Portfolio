196f97b209f7aa2fe004f01221a13d8b7ca55f6a
EhsanNoori <ehsannoori528@gmail.com>
2025-08-31 09:07:00 +0430

    bug fexes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import Image from "next/image";
 import { User } from 'lucide-react';
+import Link from 'next/link';
 
 
 export default function Navbar() {
@@ -14,11 +15,11 @@ export default function Navbar() {
      
       <a className='ml-10' href="/"><Image className='w-25 text-2xl font-bold text-green-600 cursor-pointer h-22'  src={'/ehsan-img.png'} alt={''} width={2000} height={2000}/></a> 
         <ul className='flex gap-8 text-gray-700  font-medium'>
-            <li className="hover:text-pink-200 hover:scale-120 text-white cursor-pointer transition" ><a href="/">Home</a></li>
-            <li className="hover:text-pink-200 hover:scale-120 text-white cursor-pointer transition" ><a href="/Services">Services</a></li>
-            <li className="hover:text-pink-200 hover:scale-120 text-white cursor-pointer transition" ><a href="/">Projects</a></li>
-            <li className="hover:text-pink-200 hover:scale-120 text-white cursor-pointer transition" ><a href="/about">About</a></li>
-            <li className="hover:text-pink-200 hover:scale-120 text-white cursor-pointer transition" ><a href="/Contact">Contact</a></li>
+            <li className="hover:text-pink-200 hover:scale-120 text-white cursor-pointer transition" ><Link href="/">Home</Link></li>
+            <li className="hover:text-pink-200 hover:scale-120 text-white cursor-pointer transition" ><Link href="/Services">Services</Link></li>
+            <li className="hover:text-pink-200 hover:scale-120 text-white cursor-pointer transition" ><Link href="/">Projects</Link></li>
+            <li className="hover:text-pink-200 hover:scale-120 text-white cursor-pointer transition" ><Link href="/about">About</Link></li>
+            <li className="hover:text-pink-200 hover:scale-120 text-white cursor-pointer transition" ><Link href="/Contact">Contact</Link></li>
         </ul>
 
        
